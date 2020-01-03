edeee85f97389a9a34a9586421f72f7ee1bd3b42
Deepak42074 <51868020+Deepak42074@users.noreply.github.com>
2020-01-03 12:25:50 +0530

    Update welcome.py
    adding last two lines in previous code

--- welcome.py
+++ welcome.py
@@ -2,3 +2,6 @@ t=2
 print(t)
 print("welcome to jenkins")
 print("checking github push for build in jnekins")
+
+print("this is new branch :welocme_edit  and need to pull the changes to master branch")
+print(" new branh crration and pull successful")
